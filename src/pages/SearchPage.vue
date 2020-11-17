429726334002a7b26754bcc1c65cfb87811d7460
WhatTheFar <jakpat.m@gmail.com>
2020-11-18 02:47:43 +0700

    refactor(page): rename dir from 'page' to 'pages'

---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-screen flex justify-center items-center bg-gray-500">
+    HI
+    <!-- <a-select -->
+    <!--   ref="select" -->
+    <!--   v-model:value="value1" -->
+    <!--   style="width: 120px" -->
+    <!--   @focus="focus" -->
+    <!--   @change="handleChange" -->
+    <!-- > -->
+    <!--   <a-select-option value="jack"> Jack </a-select-option> -->
+    <!--   <a-select-option value="lucy"> Lucy </a-select-option> -->
+    <!--   <a-select-option value="disabled" disabled> Disabled </a-select-option> -->
+    <!--   <a-select-option value="Yiminghe"> yiminghe </a-select-option> -->
+    <!-- </a-select> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {},
+});
+</script>
