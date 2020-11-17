0f775105cd55008e6ce0b60f8d29a3a6a02c87cb
WhatTheFar <jakpat.m@gmail.com>
2020-11-18 03:05:00 +0700

    feat: init ant-design-vue

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
+
 import { router } from '/@/router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(Antd).use(router).mount('#app');
