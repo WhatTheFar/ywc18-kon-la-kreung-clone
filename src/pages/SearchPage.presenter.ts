e3ada0b892c72bc4e9cf0b4ea0bcbed22e374d67
WhatTheFar <jakpat.m@gmail.com>
2020-11-19 08:20:19 +0700

    feat(p/search): init SearchPagePresenter

---
+++ src/pages/SearchPage.presenter.ts
@@ -0,0 +1,24 @@
+import { SearchPageViewModel } from './SearchPage.viewmodel';
+import { Merchant } from '/@/domain/entity/merchant.entity';
+import { PriceRange } from '/@/domain/usecase/search/price';
+import { SearchOutput } from '/@/domain/usecase/search/search.interactor';
+
+export class SearchPagePresenter implements SearchOutput {
+  constructor(private readonly vm: SearchPageViewModel) {}
+
+  public setCategories(categories: string[]): void {
+    this.vm.categories.value = categories;
+  }
+
+  public setSubcategories(subcategories: string[]): void {
+    this.vm.subcategories.value = subcategories;
+  }
+
+  public setPriceRangeTexts(priceRages: [string, PriceRange][]): void {
+    this.vm.priceRanges.value = priceRages;
+  }
+
+  public setSearchedMerchants(merchants: Merchant[]): void {
+    this.vm.merchants.value = merchants;
+  }
+}
